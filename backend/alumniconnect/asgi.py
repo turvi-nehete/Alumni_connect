e0a3642614ed90b7aa3df0643df05ec4fc138d0c
maheck9 <maheksanghvi2006@gmail.com>
2026-02-07 07:56:02 +0530

    Fix ASGI initialization order

--- backend/alumniconnect/asgi.py
+++ backend/alumniconnect/asgi.py
@@ -1,14 +1,18 @@
 import os
-from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
-from channels.auth import AuthMiddlewareStack
-
-import alumniconnect.routing
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "alumniconnect.settings")
 
+# Initialize Django ASGI application early to ensure the AppRegistry is populated
+# before importing code that may import ORM models.
+django_asgi_app = get_asgi_application()
+
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.auth import AuthMiddlewareStack
+import alumniconnect.routing
+
 application = ProtocolTypeRouter({
-    "http": get_asgi_application(),
+    "http": django_asgi_app,
     "websocket": AuthMiddlewareStack(
         URLRouter(
             alumniconnect.routing.websocket_urlpatterns
